16a1fe1df65f0d7819e8cbda7f70768ef9ab1165
Alan <alanmoreno220704@gmail.com>
2026-05-19 10:02:23 -0600

    Tercera version:EquipoV5reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import Equipo from './pages/Equipo/Equipo';
 import Crud from './pages/crud/Crud';
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import MiComponente from './components/micomponente.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <MiComponente />
   </StrictMode>,
 )
